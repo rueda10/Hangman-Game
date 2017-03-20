7c3e233f39d2909ba84d52e51d3fe395818ca049
Jaime Rueda <rueda10@Jaimes-MacBook-Pro.local>
2017-03-20 17:03:14 -0500

    added audio, and finished basic functionality of app

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// words
+// WORDS ARRAY
 var words = [
   "blindside",
   "tribe",
@@ -17,38 +17,99 @@ var words = [
   "outlast",
   "outwit",
   "outplay",
-  "million"
+  "million",
+  "twist",
+  "outcast",
+  "exile"
 ]
 
+// VARIABLES
 var wins = 0;
 var guessesRemaining = 15;
 var lettersGuessed = [];
 var isGameInProgress = false;
 var currentWord = "";
-
+var audio = new Audio("./assets/sounds/readygo.mp3");
+var wonMessage = "Congratulations! You guessed the word ";
+var lostMessage = "Game over, the tribe has spoken. The word was ";
+// Buttons
 var newGameButton = document.getElementById("new-game-button");
+var keepPlayingButton = document.getElementById("keep-playing-button");
+var stopPlayingButton = document.getElementById("stop-playing-button");
 
+/**
+ * Start Game button handler. Removes initial div
+ * and displays game div. Starts countdown with a
+ * delay to match audio clip
+ */
 newGameButton.onclick = function() {
-    toggleDiv("new-game");
-    toggleDiv("game-on");
+    toggleDiv("new-game", "hidden");
+    toggleDiv("ready", "hidden");
+    wins = 0;
+    audio.play();
+    setTimeout(startCountdown, 750);
+};
+
+/**
+ * Start Game button handler. Removes initial div
+ * and displays game div
+ */
+keepPlayingButton.onclick = function() {
+    toggleDiv("game-over", "hidden");
+    toggleDiv("game-on", "hidden");
     startGame();
 };
 
+/**
+ * Start Game button handler. Removes initial div
+ * and displays game div
+ */
+stopPlayingButton.onclick = function() {
+    toggleDiv("game-over", "hidden");
+    toggleDiv("new-game", "hidden");
+};
+
+/**
+ * Initalizes game settings. Gets new random word,
+ * resets guessesRemaining, and displays data in DOM
+ */
 function startGame() {
+  isGameInProgress = true;
   var wordIndex = pickRandomWord();
   currentWord = words[wordIndex];
+  guessesRemaining = 15;
+  lettersGuessed = [];
 
   guessWord();
+  setNumberOfWins();
   setNumberOfGuessesRemaining();
   setLettersAlreadyGuessed();
-  isGameInProgress = true;
 }
 
-function endGame() {
+/**
+ * Populates game over div with passed in message
+ */
+function gameOver(message) {
   isGameInProgress = false;
+  toggleDiv("game-on", "hidden");
+  toggleDiv("game-over", "hidden");
 
+  var messageSpan = document.getElementById("game-over-message");
+  var wordSpan = document.getElementById("guessed-word");
+
+  messageSpan.innerHTML = message;
+  wordSpan.innerHTML = "'" + currentWord + "'";
 }
 
+/**
+ * onkeyup event that adds letter pressed to
+ * lettersGuessed array and determines whether
+ * the character is part of the current word
+ * being guessed or not. If so, it displays it
+ * in the DOM. If not, guessesRemaining is
+ * decreased by one. Game will be over when
+ * guessesRemaining reaches 0.
+ */
 document.onkeyup = function(event) {
   if (isGameInProgress) {
     var letter = event.key;
@@ -61,6 +122,9 @@ document.onkeyup = function(event) {
         guessWord();
       } else {
         guessesRemaining--;
+        if (guessesRemaining === 0) {
+          gameOver(lostMessage);
+        }
       }
     }
 
@@ -69,8 +133,66 @@ document.onkeyup = function(event) {
   }
 }
 
-// HELPER FUNCTIONS
+/**
+ * Audio event listener that starts the game 4
+ * seconds into the audio clip. Countdown is done
+ * in a weird way because of the way audio's
+ * timeupdate works.
+ */
+// audio.addEventListener("timeupdate", function(){
+//   var currentTime = parseInt(audio.currentTime);
+//   var countdownSpan = document.getElementById("countdown-number");
+//
+//   if (!isGameInProgress && currentTime === 4) {
+//     toggleDiv("ready", "hidden");
+//     toggleDiv("game-on", "hidden");
+//     startGame();
+//   } else {
+//     if (countdownSpan.innerHTML != currentTime) {
+//       if (currentTime === 1) {
+//         countdownSpan.innerHTML = "3";
+//       } else if (currentTime === 2) {
+//         countdownSpan.innerHTML = "2";
+//       } else if (currentTime === 3) {
+//         countdownSpan.innerHTML = "1";
+//       }
+//     }
+//   }
+// });
 
+/**
+ * Displays countdown and starts game
+ */
+function startCountdown() {
+  var counter = 4;
+  var countdownSpan = document.getElementById("countdown-number");
+
+  countdownSpan.innerHTML = "4";
+
+  var countdownInterval = setInterval(function() {
+    counter--;
+    if(counter === 0) {
+      clearInterval(countdownInterval);
+      toggleDiv("ready", "hidden");
+      toggleDiv("game-on", "hidden");
+      countdownSpan.innerHTML = "5";
+      startGame();
+    } else {
+      countdownSpan.innerHTML = counter;
+    }
+  }, 1000);
+}
+
+// HELPER FUNCTIONS
+/**
+ * Sets the word being guessed in the DOM using the
+ * currentWord string and lettersGuessed array. While
+ * traversing the currentWord string, if the current
+ * character is in the lettersGuessed array, display
+ * character, if it isn't, display the underscore
+ * placeholder. Game is over when all letters in the
+ * currentWord are guessed.
+ */
 function guessWord() {
   var wordDiv = document.getElementById("word");
   wordDiv.innerHTML = "";
@@ -83,15 +205,33 @@ function guessWord() {
   }
 
   if (wordDiv.innerHTML.indexOf("_") === -1) {
-    endGame();
+    wins++;
+    gameOver(wonMessage);
   }
 }
 
+/**
+ * Sets the number of wins in the DOM using the
+ * wins integer
+ */
+function setNumberOfWins() {
+  var winsSpan = document.getElementById("num-wins");
+  winsSpan.innerHTML = wins;
+}
+
+/**
+ * Sets the guesses remaning in the DOM using the
+ * guessesRemaining integer
+ */
 function setNumberOfGuessesRemaining() {
   var guessesSpan = document.getElementById("guesses");
   guessesSpan.innerHTML = guessesRemaining;
 }
 
+/**
+ * Populates the letters guessed in the DOM using the
+ * lettersGuessed array
+ */
 function setLettersAlreadyGuessed() {
   var lettersGuessedSpan = document.getElementById("letters-guessed");
   lettersGuessedSpan.innerHTML = "";
@@ -100,11 +240,18 @@ function setLettersAlreadyGuessed() {
   }
 }
 
+/**
+ * Picks a random word index from the words array
+ */
 function pickRandomWord() {
-  return Math.floor(Math.random() * ((words.length - 1) - 1));
+  return Math.floor(Math.random() * words.length);
 }
 
-function toggleDiv(id) {
+/**
+ * Toggles the given class on and off the element
+ * corresponding to the given id
+ */
+function toggleDiv(id, myClass) {
   var div = document.getElementById(id);
-  div.classList.toggle("hidden");
+  div.classList.toggle(myClass);
 }
